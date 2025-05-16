548dda2eba8b9c0baab368324c2c1b0270284005
server01-dev <server01.dev@gmail.com>
2025-05-16 20:38:05 +0530

    Update bookRoutes.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -45,4 +45,17 @@ router.delete("/deleteBook/:id", async (req, res)=>{
   }
 })
 
+router.get('/search/:name', async (req, res) => {
+  try {
+    const { name } = req.params;
+    const books = await Book.find({
+      name: { $regex: name, $options: 'i' } 
+    });
+
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err });
+  }
+});
+
 export default router;
