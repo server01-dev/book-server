360b5c5e8073de1651af00202c66854c841781df
server01-dev <server01.dev@gmail.com>
2025-05-16 20:59:40 +0530

    Update bookRoutes.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,15 +1,14 @@
-import express from 'express';
-import Book from '../models/bookModel.js';
-
+import express from "express";
+import Book from "../models/bookModel.js";
 
 const router = express.Router();
 
-router.post('/books', async (req, res) => {
+router.post("/books", async (req, res) => {
   try {
     const { name, author, publishedYear } = req.body;
 
     if (!name || !author) {
-      return res.status(400).json({ message: 'Name and author are required.' });
+      return res.status(400).json({ message: "Name and author are required." });
     }
 
     const newBook = new Book({ name, author, publishedYear });
@@ -17,48 +16,51 @@ router.post('/books', async (req, res) => {
 
     res.status(201).json(savedBook);
   } catch (error) {
-    res.status(500).json({ message: 'Failed to add book', error: error.message });
+    res
+      .status(500)
+      .json({ message: "Failed to add book", error: error.message });
   }
 });
 
-router.get("/getBooks", async (req, res)=>{
-  try{
+router.get("/getBooks", async (req, res) => {
+  try {
     const AllBooks = await Book.find();
     res.status(200).json(AllBooks);
-  } catch (error){
-    res.status(500).json({ message: 'Failed to get all books', error: error.message });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to get all books", error: error.message });
   }
-})
+});
 
-router.delete("/deleteBook/:id", async (req, res)=>{
+router.delete("/deleteBook/:id", async (req, res) => {
   const { id } = req.params;
-   try {
+  try {
     const deletedBook = await Book.findByIdAndDelete(id);
 
     if (!deletedBook) {
-      return res.status(404).json({ message: 'Book not found' });
+      return res.status(404).json({ message: "Book not found" });
     }
 
-    res.status(200).json({ message: 'Book deleted successfully', deletedBook });
+    res.status(200).json({ message: "Book deleted successfully", deletedBook });
   } catch (error) {
-    res.status(500).json({ message: 'Server error', error });
+    res.status(500).json({ message: "Server error", error });
   }
-})
+});
 
-router.get('/search/:name', async (req, res) => {
+router.get("/search/:name", async (req, res) => {
   try {
     const { name } = req.params;
     const books = await Book.find({
-      name: { $regex: name, $options: 'i' } 
+      name: { $regex: name, $options: "i" },
     });
     if (books.length == 0) {
       res.json({ message: "No Book Found" });
     } else {
       res.json(books);
     }
-    
   } catch (err) {
-    res.status(500).json({ message: 'Server error', error: err });
+    res.status(500).json({ message: "Server error", error: err });
   }
 });
 
